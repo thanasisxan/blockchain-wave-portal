fd9f51407e6030de294c32a5b1a6188fc22caa44
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-10 22:27:55 +0300

    initial commit

---
+++ scripts/run.js
@@ -0,0 +1,56 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveStoreMap = {};
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    console.log(`Total waves before: ${waveCount}`);
+
+    const rndTimes1 = Math.floor(Math.random() * 30) + 1;
+    const rndTimes2 = Math.floor(Math.random() * 30) + 1;
+
+    for (let i = 0; i < rndTimes1; i++) {
+        let waveTxn = await waveContract.wave();
+        await waveTxn.wait();
+        if (waveTxn.from in waveStoreMap) {
+            waveStoreMap[waveTxn.from] += 1;
+        } else {
+            waveStoreMap[waveTxn.from] = 1;
+        }
+    }
+    for (let i = 0; i < rndTimes2; i++) {
+        let waveTxn = await waveContract.connect(randomPerson).wave();
+        await waveTxn.wait();
+        if (waveTxn.from in waveStoreMap) {
+            waveStoreMap[waveTxn.from] += 1;
+        } else {
+            waveStoreMap[waveTxn.from] = 1;
+        }
+    }
+    console.log(waveStoreMap);
+    waveCount = await waveContract.getTotalWaves();
+    console.log(`Total waves after: ${waveCount}`);
+
+    let topWaverAddr = Object.keys(waveStoreMap).reduce((waverAddrPrev, waverAddrAfter) => waveStoreMap[waverAddrPrev] > waveStoreMap[waverAddrAfter] ? waverAddrPrev : waverAddrAfter);
+
+    console.log(`Champion waver!: ${topWaverAddr}`);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
