dc3d4ee4c090bfdfe71a009e910a29651064c56a
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-11 16:00:00 +0300

    deploy smart contract to goerli ethereum testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -17,5 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+    solidity: "0.8.4",
+    networks: {
+        goerli: {
+            url: process.env.STAGING_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        },
+        mainnet: {
+            chainId: 1,
+            url: process.env.PROD_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY],
+        }
+    }
 };
