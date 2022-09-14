f579b71cc799a55d6324ec3b845f7fe6c4a1eab6
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-15 01:25:07 +0300

    get tiny amount of goerliEth if you wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -28,29 +28,25 @@ contract WavePortal {
      */
     Wave[] waves;
 
-    constructor() {
+    constructor() payable {
         console.log("I AM SMART CONTRACT. POG.");
     }
 
-    /*
-     * You'll notice I changed the wave function a little here as well and
-     * now it requires a string called _message. This is the message our user
-     * sends us from the frontend!
-     */
     function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
+        console.log("%s has waved!", msg.sender);
 
-        /*
-         * This is where I actually store the wave data in the array.
-         */
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        /*
-         * I added some fanciness here, Google it and try to figure out what it is!
-         * Let me know what you learn in #general-chill-chat
-         */
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     /*
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,26 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
+    console.log("Contract addy:", waveContract.address);
 
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+
+    /*
+    * Get Contract balance
+    */
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
 
     let waveStoreMap = {};
     let waveCount;
@@ -37,10 +52,18 @@ const main = async () => {
     console.log(waveStoreMap);
     waveCount = await waveContract.getTotalWaves();
     console.log(`Total waves after: ${waveCount}`);
+
+
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
     let topWaverAddr = Object.keys(waveStoreMap).reduce((waverAddrPrev, waverAddrAfter) => waveStoreMap[waverAddrPrev] > waveStoreMap[waverAddrAfter] ? waverAddrPrev : waverAddrAfter);
-
+    /*
+    * Get Contract balance to see what happened!
+    */
+    console.log(
+        "Contract balance after:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
     console.log(`Champion waver!: ${topWaverAddr}`);
 };
 
