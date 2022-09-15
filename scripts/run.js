34266aec16708f502dc15465737d7dab791c9c8c
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-15 03:12:16 +0300

    updated contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,5 +1,4 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy({
         value: hre.ethers.utils.parseEther("0.1"),
@@ -7,12 +6,6 @@ const main = async () => {
     await waveContract.deployed();
     console.log("Contract addy:", waveContract.address);
 
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-
-    /*
-    * Get Contract balance
-    */
     let contractBalance = await hre.ethers.provider.getBalance(
         waveContract.address
     );
@@ -21,50 +14,23 @@ const main = async () => {
         hre.ethers.utils.formatEther(contractBalance)
     );
 
+    /*
+     * Let's try two waves now
+     */
+    const waveTxn = await waveContract.wave("This is wave #1");
+    await waveTxn.wait();
 
-    let waveStoreMap = {};
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
 
-    console.log(`Total waves before: ${waveCount}`);
-
-    const rndTimes1 = Math.floor(Math.random() * 30) + 1;
-    const rndTimes2 = Math.floor(Math.random() * 30) + 1;
-
-    for (let i = 0; i < rndTimes1; i++) {
-        let waveTxn = await waveContract.wave(`test wave ${i + rndTimes1}`);
-        await waveTxn.wait();
-        if (waveTxn.from in waveStoreMap) {
-            waveStoreMap[waveTxn.from] += 1;
-        } else {
-            waveStoreMap[waveTxn.from] = 1;
-        }
-    }
-    for (let i = 0; i < rndTimes2; i++) {
-        let waveTxn = await waveContract.connect(randomPerson).wave(`test wave ${i + rndTimes2}`);
-        await waveTxn.wait();
-        if (waveTxn.from in waveStoreMap) {
-            waveStoreMap[waveTxn.from] += 1;
-        } else {
-            waveStoreMap[waveTxn.from] = 1;
-        }
-    }
-    console.log(waveStoreMap);
-    waveCount = await waveContract.getTotalWaves();
-    console.log(`Total waves after: ${waveCount}`);
-
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
-    let topWaverAddr = Object.keys(waveStoreMap).reduce((waverAddrPrev, waverAddrAfter) => waveStoreMap[waverAddrPrev] > waveStoreMap[waverAddrAfter] ? waverAddrPrev : waverAddrAfter);
-    /*
-    * Get Contract balance to see what happened!
-    */
-    console.log(
-        "Contract balance after:",
-        hre.ethers.utils.formatEther(contractBalance)
-    );
-    console.log(`Champion waver!: ${topWaverAddr}`);
 };
 
 const runMain = async () => {
